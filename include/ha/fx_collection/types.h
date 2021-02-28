eb00e2035b78ff050a56bb17f3dd55e54e85444b
René Hansen <rene@hansen-audio.de>
2021-02-28 18:31:11 +0100

    Add ZERO_VECTOR for initialisation

--- include/ha/fx_collection/types.h
+++ include/ha/fx_collection/types.h
@@ -9,11 +9,13 @@
 namespace ha {
 namespace fx_collection {
 //------------------------------------------------------------------------
+constexpr std::size_t VECTOR_SIZE    = 4;
 constexpr std::size_t BYTE_ALIGNMENT = 16;
 using i32                            = std::int32_t;
 using float_t                        = float;
-using vector_float =
-    std::vector<float_t, ha::alignment::aligned_allocator<float_t, BYTE_ALIGNMENT>>;
+using vector_float = std::vector<float_t, alignment::aligned_allocator<float_t, BYTE_ALIGNMENT>>;
+
+static const vector_float ZERO_VECTOR(VECTOR_SIZE, float_t(0.));
 
 //------------------------------------------------------------------------
 } // namespace fx_collection
