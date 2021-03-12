d2d34170b23e95391607847c5fadbebcf3fd7b74
René Hansen <rene@hansen-audio.de>
2021-03-12 17:00:22 +0100

    Replace aligned vector by aligned array. Disable aligned_allocator dependency

--- include/ha/fx_collection/types.h
+++ include/ha/fx_collection/types.h
@@ -2,20 +2,20 @@
 
 #pragma once
 
-#include "ha/aligned_allocator/allocator.h"
+#include <array>
 #include <cstdint>
 #include <vector>
 
 namespace ha {
 namespace fx_collection {
 //------------------------------------------------------------------------
-constexpr std::size_t VECTOR_SIZE    = 4;
-constexpr std::size_t BYTE_ALIGNMENT = 16;
-using i32                            = std::int32_t;
-using float_t                        = float;
-using vector_float = std::vector<float_t, alignment::aligned_allocator<float_t, BYTE_ALIGNMENT>>;
+using i32     = std::int32_t;
+using float_t = float;
 
-static const vector_float ZERO_VECTOR(VECTOR_SIZE, float_t(0.));
+constexpr std::size_t ARRAY_SIZE           = 4;
+constexpr std::size_t BYTE_ALIGNMENT       = 16;
+using vector_float alignas(BYTE_ALIGNMENT) = std::array<float_t, ARRAY_SIZE>;
+constexpr vector_float ZERO_ARRAY{0., 0., 0., 0.};
 
 //------------------------------------------------------------------------
 } // namespace fx_collection
