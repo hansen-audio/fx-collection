1be7b8858b7f82d82b3f9c4c444618418016098a
René Hansen <rene@hansen-audio.de>
2021-05-12 22:50:02 +0200

    Add some unit tests

--- test/trance_gate_test.cpp
+++ test/trance_gate_test.cpp
@@ -9,10 +9,66 @@ using namespace ha::fx_collection;
 namespace {
 
 //-----------------------------------------------------------------------------
-TEST(trance_gate_test, test_instantiation)
+TEST(trance_gate_test, test_setting_mix)
 {
-    auto tg_context = trance_gate::create();
-    trance_gate::set_mix(tg_context, 1.);
+    auto cx = trance_gate::create();
+    trance_gate::set_sample_rate(cx, real(44100.));
+    trance_gate::set_mix(cx, 1.);
+    EXPECT_EQ(cx.mix, 1.);
+}
+
+//-----------------------------------------------------------------------------
+TEST(trance_gate_test, test_delay_active)
+{
+    auto cx = trance_gate::create();
+    trance_gate::set_sample_rate(cx, real(44100.));
+    EXPECT_FALSE(cx.is_delay_active);
+    trance_gate::trigger(cx, real(1. / 32.));
+    EXPECT_TRUE(cx.is_delay_active);
+}
+
+//-----------------------------------------------------------------------------
+TEST(trance_gate_test, test_fade_in_active)
+{
+    auto cx = trance_gate::create();
+    trance_gate::set_sample_rate(cx, real(44100.));
+    EXPECT_FALSE(cx.is_fade_in_active);
+    trance_gate::trigger(cx, real(0.), real(1. / 32.));
+    EXPECT_TRUE(cx.is_fade_in_active);
+}
+
+//-----------------------------------------------------------------------------
+TEST(trance_gate_test, test_processing)
+{
+    constexpr i32 LEFT_CH = 0;
+    auto cx               = trance_gate::create();
+    trance_gate::set_sample_rate(cx, real(44100.));
+    trance_gate::set_step_count(cx, 8);
+    trance_gate::set_step(cx, LEFT_CH, 0, real(1.));
+    trance_gate::set_step(cx, LEFT_CH, 1, real(0.));
+    trance_gate::set_step(cx, LEFT_CH, 2, real(1.));
+    trance_gate::set_step(cx, LEFT_CH, 3, real(0.));
+    trance_gate::set_step(cx, LEFT_CH, 4, real(1.));
+    trance_gate::set_step(cx, LEFT_CH, 5, real(1.));
+    trance_gate::set_step(cx, LEFT_CH, 6, real(1.));
+    trance_gate::set_step(cx, LEFT_CH, 7, real(0.));
+    trance_gate::set_step_len(cx, real(1. / 32.));
+
+    mut_i32 numSamples = 1024;
+    audio_frame sum{real(0.), real(0.), real(0.), real(0.)};
+    while (numSamples-- > 0)
+    {
+        audio_frame in{real(0.5), real(0.5), real(0.), real(0.)};
+        audio_frame out{real(0.), real(0.), real(0.), real(0.)};
+        trance_gate::process(cx, in, out);
+        for (mut_i32 i = 0; i < sum.data.size(); ++i)
+            sum.data[i] += out.data[i];
+    }
+
+    EXPECT_TRUE(sum.data[0] > real(468.));
+    EXPECT_TRUE(sum.data[1] > real(468.));
+    EXPECT_TRUE(sum.data[0] < real(469.));
+    EXPECT_TRUE(sum.data[1] < real(469.));
 }
 
 //-----------------------------------------------------------------------------
