e8d154c0b089110fcfd0a81fa6459662f3ced3ff
René Hansen <rene@hansen-audio.de>
2021-05-29 22:22:57 +0200

    Add some docu to is_shuffle_note function

--- source/detail/shuffle_note.h
+++ source/detail/shuffle_note.h
@@ -7,6 +7,13 @@
 namespace ha::fx_collection::detail {
 
 //-----------------------------------------------------------------------------
+/**
+ * @brief Every second 16th note is a shuffle note.
+ *
+ * @param note_index Index of the note inside the pattern starting from 0
+ * @param note_len Length of the note
+ * @return Returns true, if index is a shuffle note
+ */
 bool is_shuffle_note(i32 note_index, real note_len);
 
 //-----------------------------------------------------------------------------
